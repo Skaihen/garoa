838b73528bb879825fe9f6d5945632361fc0a2dd
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-18 11:03:27 +0200

    model

--- core/src/io/skaihen/Garoa.java
+++ core/src/io/skaihen/Garoa.java
@@ -1,31 +1,69 @@
 package io.skaihen;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class Garoa extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	public Environment environment;
+	public PerspectiveCamera perspectiveCamera;
+	public CameraInputController cameraController;
+	public Model model;
+	public ModelBatch modelBatch;
+	public ModelInstance instance;
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		modelBatch = new ModelBatch();
+
+		perspectiveCamera = new PerspectiveCamera(90, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		perspectiveCamera.position.set(1f, 1f, 1f);
+		perspectiveCamera.lookAt(0, 0, 0);
+		perspectiveCamera.near = 1f;
+		perspectiveCamera.far = 300f;
+		perspectiveCamera.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				Usage.Position | Usage.Normal);
+		instance = new ModelInstance(model);
+
+		cameraController = new CameraInputController(perspectiveCamera);
+		Gdx.input.setInputProcessor(cameraController);
 	}
 
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		cameraController.update();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(perspectiveCamera);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
+
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		img.dispose();
+		modelBatch.dispose();
+		model.dispose();
 	}
 }
--- desktop/src/io/skaihen/DesktopLauncher.java
+++ desktop/src/io/skaihen/DesktopLauncher.java
@@ -1,14 +1,32 @@
 package io.skaihen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+
 		config.setTitle("Garoa");
+
+		config.setWindowedMode(800, 480);
+
+		config.setWindowListener(new Lwjgl3WindowAdapter() {
+			@Override
+			public void focusLost() {
+				Gdx.graphics.setForegroundFPS(60);
+			}
+
+			@Override
+			public void focusGained() {
+				Gdx.graphics.setForegroundFPS(10);
+			}
+		});
+		config.useVsync(true);
+
 		new Lwjgl3Application(new Garoa(), config);
 	}
 }
