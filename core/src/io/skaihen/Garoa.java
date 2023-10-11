f15ae5b789846f34204f8c7e075b7184ee240112
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-11 11:06:42 +0200

    cam

--- core/src/io/skaihen/Garoa.java
+++ core/src/io/skaihen/Garoa.java
@@ -1,23 +1,36 @@
 package io.skaihen;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Garoa extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private int widthScreen, heightScreen;
+	private OrthographicCamera orthographicCamera;
 	
 	@Override
 	public void create () {
+		widthScreen = Gdx.graphics.getWidth();
+		heightScreen = Gdx.graphics.getHeight();
+		orthographicCamera = new OrthographicCamera(widthScreen, heightScreen);
+		orthographicCamera.setToOrtho(false, widthScreen, heightScreen);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("plep.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		orthographicCamera.update();
+
+		batch.setProjectionMatrix(orthographicCamera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
