b47cfe3724d20191a3860284b09de64bf364ba56
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-16 11:35:30 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
-    anim8 = require("libraries/anim8.lua")
+    anim8 = require("libraries/anim8")
+    love.graphics.setDefaultFilter("nearest", "nearest")
 
     player = {}
     player.x = 400
@@ -10,28 +11,47 @@ function love.load()
 
     player.animations = {}
     player.animations.down = anim8.newAnimation(player.grid("1-4", 1), 0.2)
+    player.animations.left = anim8.newAnimation(player.grid("1-4", 2), 0.2)
+    player.animations.right = anim8.newAnimation(player.grid("1-4", 3), 0.2)
+    player.animations.up = anim8.newAnimation(player.grid("1-4", 4), 0.2)
+
+    player.anim = player.animations.down
 
     background = love.graphics.newImage("assets/world/test.png")
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("right") then
-        player.x = player.x + player.speed
-    end
-    if love.keyboard.isDown("left") then
-        player.x = player.x - player.speed
-    end
-    if love.keyboard.isDown("up") then
-        player.y = player.y - player.speed
-    end
-    if love.keyboard.isDown("down") then
+    local isMoving = false
+
+    if love.keyboard.isDown("s") then
         player.y = player.y + player.speed
+        player.anim = player.animations.down
+        isMoving = true
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+        player.anim = player.animations.left
+        isMoving = true
+    end
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+        player.anim = player.animations.right
+        isMoving = true
+    end
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+        player.anim = player.animations.up
+        isMoving = true
     end
 
-    player.animations.down:update(dt)
+    if isMoving == false then
+        player.anim:gotoFrame(2)
+    end
+
+    player.anim:update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0)
-    player.animations.down:draw(player.spriteSheet, player.x, player.y, nil, 10)
+    love.graphics.draw(background, 0, 0, nil, 4)
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 4)
 end
