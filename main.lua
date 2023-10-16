5fa81c09158e56260052d21f2e8916a5b4a2df90
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-16 11:11:30 +0200

    libs

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
 function love.load()
+    anim8 = require("libraries/anim8.lua")
+
     player = {}
     player.x = 400
     player.y = 200
     player.speed = 3
+    player.spriteSheet = love.graphics.newImage("assets/sprites/player-sheet.png")
+    player.grid = anim8.newGrid(12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation(player.grid("1-4", 1), 0.2)
+
+    background = love.graphics.newImage("assets/world/test.png")
 end
 
 function love.update(dt)
@@ -18,8 +27,11 @@ function love.update(dt)
     if love.keyboard.isDown("down") then
         player.y = player.y + player.speed
     end
+
+    player.animations.down:update(dt)
 end
 
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, 50)
+    love.graphics.draw(background, 0, 0)
+    player.animations.down:draw(player.spriteSheet, player.x, player.y, nil, 10)
 end
