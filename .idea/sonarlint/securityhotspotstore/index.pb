61a431eafa7f6c5587178ce2f4205126e2d8ef77
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-17 17:53:34 +0200

    python

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -2,15 +2,13 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-B
-libraries/bump.lua,e\d\eda7c997ee5b5b5698d082169cdafff3bdc25ead
-8
-main.lua,e\9\e95de2280f70e0dcf3f3bb7a58ecd9d18bfb8510
-=
-reference.lua,8\2\8242591345cc9c16e9b692b54d453bc59d50b684
 @
 requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
 <
 old/main.lua,9\b\9bb3d62a29084d2609e6c457b8e9f82b6909420f
 ;
 __main__.py,f\6\f62d74e05be5f818f1b288b6653219b695385c68
+7
+LICENSE,0\3\0398ccd0f49298b10a3d76a47800d2ebecd49859
+8
+Garoa.py,5\1\5172744e0bc26055ba226eef12d2a8955404c802
---
+++ Garoa.py
@@ -0,0 +1,82 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Garoa"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 2
+
+PLAYER_MOVEMENT_SPEED = 5
+
+
+class Garoa(arcade.Window):
+
+    def __init__(self, width, height, title, vsync):
+        super().__init__(width, height, title, vsync)
+
+        self.scene = None
+        self.player_sprite = None
+        self.physics_engine = None
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        self.scene = arcade.Scene()
+
+        self.player_sprite = arcade.Sprite("assets/sprites/player-sheet.png", CHARACTER_SCALING)
+        self.player_sprite.center_x = 12 * CHARACTER_SCALING
+        self.player_sprite.center_y = 18 * CHARACTER_SCALING
+
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        wall = arcade.Sprite("assets/sprites/player-sheet.png", TILE_SCALING)
+        wall.center_x = 200
+        wall.center_y = 200
+        self.scene.add_sprite("Walls", wall)
+
+        # self.player_sprite.textures = []
+        #
+        # self.player_sprite.textures.append(
+        #     arcade.load_texture("assets/sprites/player-sheet.png", x=0, y=0, width=12, height=18))
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )
+
+    def on_draw(self):
+        self.clear()
+        self.scene.draw(pixelated=True)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+
+def main():
+    window = Garoa(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, False)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
