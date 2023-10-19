fa1cd6090142134e1231191bb4c57c17e2145e76
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-19 12:48:04 +0200

    sprite anim

--- Garoa.py
+++ Garoa.py
@@ -9,15 +9,16 @@ SCREEN_HEIGHT = 350
 SCREEN_TITLE = "Garoa"
 
 TILE_SCALING = 0.5
-CHARACTER_SCALING = TILE_SCALING * 2
+CHARACTER_SCALING = TILE_SCALING * 4
 SPRITE_PIXEL_SIZE = 64
 GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
 
-PLAYER_MOVEMENT_SPEED = 5
+PLAYER_MOVEMENT_SPEED = 3
 
-PLAYER_START_X = 90 * CHARACTER_SCALING
-PLAYER_START_Y = 800 * CHARACTER_SCALING
+PLAYER_START_X = 4
+PLAYER_START_Y = 25
 
+PLAYER_LAYER = "Player"
 BACKGROUND_LAYER = "Background"
 TREES_LAYER = "Trees"
 ITEMS_LAYER = "Items"
@@ -50,11 +51,15 @@ class Garoa(arcade.Window):
         self.tile_map = arcade.TileMap("assets/maps/testMap.json", TILE_SCALING, layer_options)
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
-        self.scene.add_sprite_list_after("Player", TREES_LAYER)
-        self.player_sprite = Character("assets/sprites/player-sheet.png")
-        self.player_sprite.center_x = PLAYER_START_X
-        self.player_sprite.center_y = PLAYER_START_Y
-        self.scene.add_sprite("Player", self.player_sprite)
+        self.player_sprite = Character("player-sheet.png", 12,
+                                       18, 4, 16, CHARACTER_SCALING)
+        self.player_sprite.center_x = (
+                self.tile_map.tile_width * TILE_SCALING * PLAYER_START_X
+        )
+        self.player_sprite.center_y = (
+                self.tile_map.tile_height * TILE_SCALING * PLAYER_START_Y
+        )
+        self.scene.add_sprite(PLAYER_LAYER, self.player_sprite)
 
         self.map_height = self.tile_map.height * GRID_PIXEL_SIZE
         if self.tile_map.background_color:
@@ -70,7 +75,7 @@ class Garoa(arcade.Window):
         self.scene.draw(pixelated=True)
         self.gui_camera.use()
 
-        score_text = f"Interfaz {self.player_sprite.center_y + (self.camera.viewport_height / 2)}"
+        score_text = f"Interfaz {self.player_sprite.change_x}, {self.player_sprite.change_y}"
         arcade.draw_text(
             score_text,
             10,
@@ -115,6 +120,10 @@ class Garoa(arcade.Window):
 
     def on_update(self, delta_time):
         self.physics_engine.update()
+        self.scene.update_animation(
+            delta_time, [BACKGROUND_LAYER, PLAYER_LAYER]
+        )
+        # self.scene.update([ITEMS_LAYER])
         self.center_camera_to_player()
 
 
--- models/Character.py
+++ models/Character.py
@@ -5,28 +5,32 @@ import arcade
 
 
 class Character(arcade.Sprite):
-    def __init__(self, file_name: Union[str, Path], sprite_width: int, sprite_height: int, columns: int, count: int):
-        super().__init__()
+    def __init__(self, name_file: Union[str, Path], sprite_width: int, sprite_height: int, columns: int, count: int,
+                 scale: float):
+        super().__init__(scale=scale)
 
-        self.walk_textures = arcade.load_spritesheet(file_name, sprite_width, sprite_height, columns, count)
+        main_path = f"assets/sprites/{name_file}"
+        self.walk_textures = arcade.load_spritesheet(main_path, sprite_width, sprite_height, columns, count)
         self.columns = columns
-        self.character_face_direction = 0
         self.cur_texture = 0
+        self.character_face_direction = 0
+        self.texture = self.walk_textures[1]
+        self.hit_box = self.texture.hit_box_points
 
     def update_animation(self, delta_time: float = 1 / 60):
-        if self.change_x < 0:
-            self.character_face_direction = 4
-        elif self.change_x > 0:
-            self.character_face_direction = 8
-
-        if self.change_y < 0:
-            self.character_face_direction = 0
-        elif self.change_y > 0:
-            self.character_face_direction = 12
-
-        if self.change_x == 0 and self.change_y == 0:
-            self.texture = self.walk_textures[self.character_face_direction]
-            return
+        # if self.change_x < 0:
+        #     self.character_face_direction = 4
+        # elif self.change_x > 0:
+        #     self.character_face_direction = 8
+        #
+        # if self.change_y < 0:
+        #     self.character_face_direction = 0
+        # elif self.change_y > 0:
+        #     self.character_face_direction = 12
+        #
+        # if self.change_x == 0 and self.change_y == 0:
+        #     self.texture = self.walk_textures[self.character_face_direction]
+        #     return
 
         self.cur_texture += 1
         if self.cur_texture > self.columns:
