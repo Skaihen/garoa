fbc467ce6593009853975d1efb39803a81f12a1b
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-10-31 14:55:50 +0100

    Update health_bar.py

--- garoa/models/health_bar.py
+++ garoa/models/health_bar.py
@@ -4,17 +4,15 @@ import arcade
 
 
 class HealthBar(arcade.Sprite):
-    def __init__(self, sprite_list: arcade.SpriteList, position: Tuple[float, float] = (0, 0),
+    def __init__(self, max_health: int, position: Tuple[float, float] = (0, 0),
                  full_color: arcade.Color = arcade.color.GREEN, background_color: arcade.Color = arcade.color.BLACK,
                  width: int = 100, height: int = 4, border_size: int = 4) -> None:
         super().__init__()
-        self.sprite_list = sprite_list
-
+        self.max_health = max_health
         self.box_width = width
         self.box_height = height
-        self.center_x = 0.0
-        self.center_y = 0.0
-        self.fullness = 5
+        self.center_x = position[0]
+        self.center_y = position[1]
 
         self.background_box = arcade.SpriteSolidColor(
             self.box_width + border_size,
@@ -26,38 +24,10 @@ class HealthBar(arcade.Sprite):
             self.box_height,
             full_color,
         )
+        self.sprite_list = arcade.SpriteList()
         self.sprite_list.append(self.background_box)
         self.sprite_list.append(self.full_box)
-        self.position = position
 
-    @property
-    def fullness(self) -> float:
-        return self.fullness
+    def update_bar_on_dmg(self):
+        
 
-    @fullness.setter
-    def fullness(self, new_fullness: float) -> None:
-        if not (0.0 <= new_fullness <= self.fullness):
-            raise ValueError(
-                f"Got {new_fullness}, but fullness must be between 0.0 and {self.fullness}."
-            )
-
-        self.fullness = new_fullness
-        if new_fullness == 0:
-            self.full_box.visible = False
-        else:
-            self.full_box.visible = True
-            self.full_box.width = self.box_width * new_fullness
-            self.full_box.left = self.center_x - (self.box_width // 2)
-
-    @property
-    def position(self) -> Tuple[float, float]:
-        return self.center_x, self.center_y
-
-    @position.setter
-    def position(self, new_position: Tuple[float, float]) -> None:
-        if new_position != self.position:
-            self.center_x, self.center_y = new_position
-            self.background_box.position = new_position
-            self.full_box.position = new_position
-
-            self.full_box.left = self.center_x - (self.box_width // 2)
