e30498b510d21d9f4dfef01a9714ea220e8ab196
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-11-15 09:23:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int main(int argc, char *argv[])
-{
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Garoa", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(3000);
-
+int main(int argc, char* argv[]) {
+    // Inicializar SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Error al inicializar SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+ 
+    // Crear una ventana
+    SDL_Window* window = SDL_CreateWindow(
+        "Garoa",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+ 
+    if (window == nullptr) {
+        std::cerr << "Error al crear la ventana: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+ 
+    // Variable para controlar el bucle principal
+    bool running = true;
+    SDL_Event event;
+ 
+    while (running) {
+        // Procesar la cola de eventos
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+ 
+    // Limpiar y cerrar
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+ 
     return 0;
 }
